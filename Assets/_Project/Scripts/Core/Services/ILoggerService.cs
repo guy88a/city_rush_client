624225d43f335e45c42565bac01726b5c7710e5b
guy88a <guy88a@gmail.com>
2025-11-28 17:12:28 +0200

    quick messages logger

--- Assets/_Project/Scripts/Core/Services/ILoggerService.cs
+++ Assets/_Project/Scripts/Core/Services/ILoggerService.cs
@@ -2,6 +2,10 @@ namespace CityRush.Core.Services
 {
     public interface ILoggerService : IGameService
     {
-        void Log(string message);
+        void Success(string message);
+        void Error(string message);
+        void Warning(string message);
+        void Info(string message);
+        void Event(string message);
     }
 }
--- Assets/_Project/Scripts/Core/Services/LoggerService.cs
+++ Assets/_Project/Scripts/Core/Services/LoggerService.cs
@@ -1,10 +1,37 @@
+using UnityEngine;
+
 namespace CityRush.Core.Services
 {
     public class LoggerService : ILoggerService
     {
-        public void Log(string message)
+        public void Success(string message)
         {
-            UnityEngine.Debug.Log(message);
+            Debug.Log(Format("Success", "green", message));
+        }
+
+        public void Error(string message)
+        {
+            Debug.LogError(Format("Error", "red", message));
+        }
+
+        public void Warning(string message)
+        {
+            Debug.LogWarning(Format("Warning", "orange", message));
+        }
+
+        public void Info(string message)
+        {
+            Debug.Log(Format("Info", "#00cfff", message)); // sky-blue
+        }
+
+        public void Event(string message)
+        {
+            Debug.Log(Format("Event", "#30f0c0", message)); // turquoise
+        }
+
+        private string Format(string prefix, string color, string message)
+        {
+            return $"<b><color={color}>{prefix}:</color></b> {message}";
         }
     }
 }
