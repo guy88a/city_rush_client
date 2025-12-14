eefd9b0cd74779746fe605c460819805da1ee0b0
guy88a <guy88a@gmail.com>
2025-12-14 21:18:50 +0200

    successfuly test building row

--- Assets/_Project/Scripts/World/Street/BuildingRowComponent.cs
+++ Assets/_Project/Scripts/World/Street/BuildingRowComponent.cs
@@ -1,17 +1,53 @@
 using UnityEngine;
 using CityRush.World.Buildings.Data;
+using CityRush.World.Buildings.Generation;
 
 namespace CityRush.World.Street
 {
     public class BuildingRowComponent : MonoBehaviour
     {
-        // Ordered building definitions (left > right)
+        [SerializeField] private BuildingGenerator buildingGeneratorPrefab;
+        [SerializeField] private Transform buildingsRoot;
+
         private BuildingDefinition[] buildings;
 
-        // Entry point (called by StreetComponent later)
+        private const float ModuleWidth = 160f / 48f;
+
         public void SetBuildings(BuildingDefinition[] buildingDefinitions)
         {
             buildings = buildingDefinitions;
+            RebuildRow();
+        }
+
+        private void RebuildRow()
+        {
+            Clear();
+
+            if (buildings == null || buildingGeneratorPrefab == null)
+                return;
+
+            float currentX = 0f;
+
+            foreach (var definition in buildings)
+            {
+                var instance = Instantiate(buildingGeneratorPrefab, buildingsRoot);
+                instance.transform.localPosition = new Vector3(currentX, 0f, 0f);
+
+                instance.Build(definition);
+
+                currentX += definition.Width * ModuleWidth;
+            }
+        }
+
+        private void Clear()
+        {
+            if (buildingsRoot == null)
+                return;
+
+            for (int i = buildingsRoot.childCount - 1; i >= 0; i--)
+            {
+                DestroyImmediate(buildingsRoot.GetChild(i).gameObject);
+            }
         }
     }
 }
--- Assets/_Project/Scripts/World/Street/StreetComponent.cs
+++ Assets/_Project/Scripts/World/Street/StreetComponent.cs
@@ -8,17 +8,34 @@ namespace CityRush.World.Street
         [TextArea(5, 20)]
         [SerializeField] private string streetJson;
 
+        [SerializeField] private BuildingRowComponent buildingRow;
+
         private StreetData streetData;
+
+        private void Start()
+        {
+            if (!string.IsNullOrEmpty(streetJson))
+                BuildStreetFromJson(streetJson);
+        }
 
         public void BuildStreetFromJson(string json)
         {
             streetJson = json;
             ParseStreetData();
+            AssignBuildings();
         }
 
         private void ParseStreetData()
         {
             streetData = JsonUtility.FromJson<StreetData>(streetJson);
+        }
+
+        private void AssignBuildings()
+        {
+            if (buildingRow == null || streetData == null)
+                return;
+
+            buildingRow.SetBuildings(streetData.buildings);
         }
     }
 }
